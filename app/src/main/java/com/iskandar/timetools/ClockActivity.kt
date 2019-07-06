98c11e2591b79f0b3cbd5124663af085a653c349
iskmz <iskandarmazzawi@gmail.com>
2019-07-07 01:25:37 +0300

    done with LOCAL time json get, parse & display

--- app/src/main/java/com/iskandar/timetools/ClockActivity.kt
+++ app/src/main/java/com/iskandar/timetools/ClockActivity.kt
@@ -1,10 +1,24 @@
 package com.iskandar.timetools
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_clock.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+data class LocalTime(val timezone:String, val abbr:String, val date:String, val time:String, val utc_offset:String)
 
 class ClockActivity : AppCompatActivity() {
 
+    val context = this@ClockActivity as Context
 
     val localTimeURL = "http://worldtimeapi.org/api/ip"
     val timezonesURL = "http://worldtimeapi.org/api/timezone"
@@ -13,105 +27,68 @@ class ClockActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clock)
 
-        //loadLocalTime()
+        loadLocalTime()
         //loadTimezones()
     }
 
-
+    @SuppressLint("StaticFieldLeak")
     private fun loadLocalTime() {
+
+        var localTime:LocalTime? = null
+
+        object : AsyncTask<Void, Void, String>() {
+
+            override fun doInBackground(vararg params: Void?): String? {
+
+                // check for Internet connection first ! //
+                if(!hasInternet()) return null
+
+                // get JSON data
+                val inSt = URL(localTimeURL).content as InputStream
+                return inSt.bufferedReader().use { it.readText() }
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+
+                // parse JSON data // if NOT null
+                result?.let{
+                    val jsonObj = JSONObject(it)
+                        localTime = LocalTime(
+                            jsonObj.getString("timezone"),
+                            jsonObj.getString("abbreviation"),
+                            jsonObj.getString("datetime").split("T")[0],
+                            jsonObj.getString("datetime").split("T")[1].split(".")[0],
+                            jsonObj.getString("utc_offset")
+                        )
+                }
+
+                // set TextView with new data // if localTime NOT null
+                localTime?.let{
+                    txtLocalTime.text = StringBuilder(" timezone: ${it.timezone} , ${it.abbr}" +
+                            "\n\n date: ${it.date} \n\n time: ${it.time} , ${it.utc_offset}")
+                }
+            }
+
+        }.execute()
+
 
     }
 
     /*
+    // LOCAL TIME json example //
     {"week_number":27,"utc_offset":"+03:00","utc_datetime":"2019-07-06T20:22:12.351816+00:00","unixtime":1562444532,"timezone":"Asia/Jerusalem","raw_offset":7200,"dst_until":"2019-10-26T23:00:00+00:00","dst_offset":3600,"dst_from":"2019-03-29T00:00:00+00:00","dst":true,"day_of_year":187,"day_of_week":6,"datetime":"2019-07-06T23:22:12.351816+03:00","client_ip":"85.130.211.80","abbreviation":"IDT"}
      */
 
     private fun loadTimezones() {
 
     }
+
+
+    private fun hasInternet():Boolean {
+        // if we have internet >> true , otherwise>> false
+        val conMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo = conMgr.activeNetworkInfo
+        return netInfo != null && netInfo.isConnected
+    }
 }
-
-
-/*
-
-
-    @SuppressLint("StaticFieldLeak")
-    private void getDataJSON() {
-        new AsyncTask<Void, Void, List<ClsWeather>>() {
-            @Override
-            protected List<ClsWeather> doInBackground(Void... voids) {
-
-                HttpURLConnection connection = null;
-                // for each city, make connection, and get its own data //
-                for(int i=0; i<myWeather.size(); i+=1) {
-                    try {
-                        connection = (HttpURLConnection) new URL(
-                                myWeather.get(i).getApiURL()).openConnection();
-                        BufferedReader buf = new BufferedReader(new InputStreamReader(
-                                connection.getInputStream()));
-                        String line, jsonStringElement="";
-                        while ((line = buf.readLine()) != null) {
-                            jsonStringElement += line;
-                        }
-                        myWeather.get(i).setJsonString(jsonStringElement);
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        connection.disconnect();
-                    }
-                }
-
-                return myWeather;
-            }
-
-            @Override
-            protected void onPostExecute(List<ClsWeather> s) {
-                super.onPostExecute(s);
-                parseJson();
-            }
-        }.execute();
-    }
-
-    private void parseJson() {
-
-        for(int i=0; i<myWeather.size(); i+=1) {
-            try {
-
-                JSONObject jsonObject = new JSONObject(myWeather.get(i).getJsonString());
-                JSONObject currently = jsonObject.getJSONObject("currently");
-
-                //  getting desired data // and putting it in list
-                myWeather.get(i).setWeatherData(
-                        currently.getLong("time"),
-                        currently.getString("summary"),
-                        currently.getDouble("temperature"),
-                        (int)(currently.getDouble("humidity")*100),
-                        currently.getString("icon")
-                );
-
-
-                // Block to check icons appearance
-
-//                myWeather.get(0).setIconTxt("snow");
-//                myWeather.get(1).setIconTxt("thunderstorm");
-//                myWeather.get(2).setIconTxt("tornado");
-//                myWeather.get(3).setIconTxt("tornado");
-//                myWeather.get(4).setIconTxt("wind");
-
-                // end of check-block
-
-
-                /* adapter */
-                WeatherAdapter myAdp = new WeatherAdapter(context,myWeather,langCode);
-                lstWeather.setAdapter(myAdp);
-                txtFetchingData.setText("");
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
- */
