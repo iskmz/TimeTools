361cee9fd7628ba38df207001d8301bbb57fd8e1
iskmz <iskandarmazzawi@gmail.com>
2019-07-08 08:35:20 +0300

    countdown: start of countdown service ....

--- app/src/main/java/com/iskandar/timetools/CountdownActivity.kt
+++ app/src/main/java/com/iskandar/timetools/CountdownActivity.kt
@@ -1,8 +1,11 @@
 package com.iskandar.timetools
 
+import android.app.Service
 import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.EditText
 import android.widget.NumberPicker
 import kotlinx.android.synthetic.main.activity_countdown.*
@@ -12,9 +15,15 @@ class CountdownActivity : AppCompatActivity() {
 
     val HOURS_MAX_VALUE = 99
 
+    val HOURS = "h"
+    val MINUTES = "m"
+    val SECONDS = "s"
+
     val context = this@CountdownActivity as Context
 
     lateinit var timeCountdown:TimeCountdown
+    lateinit var countdownService: CountdownService
+    var countdownOn = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +34,7 @@ class CountdownActivity : AppCompatActivity() {
 
     private fun initialize() {
         timeCountdown = TimeCountdown()
+        countdownService = CountdownService(timeCountdown)
         setNumberPickers()
         setButtons()
     }
@@ -48,6 +58,19 @@ class CountdownActivity : AppCompatActivity() {
         }
 
         btnCountdownStart.setOnClickListener {
+            if(countdownOn){
+                stopService(Intent(context,CountdownService::class.java))
+                countdownOn=false
+            }
+            else{
+                // START / RESUME
+                intent = Intent(context,CountdownService::class.java)
+                intent.putExtra(HOURS,timeCountdown.hour)
+                intent.putExtra(MINUTES,timeCountdown.min)
+                intent.putExtra(SECONDS,timeCountdown.sec)
+                startService(Intent(context,CountdownService::class.java))
+                countdownOn=true
+            }
 
         }
     }
@@ -59,14 +82,14 @@ class TimeCountdown(var sec: Int, var min: Int, var hour: Int) {
     constructor() : this(0, 0, 0)
     constructor(other: TimeDisplay) : this(other.sec, other.min, other.hour)
 
-    open fun tick() {
-        this.sec += 1 // tick tock tick tock tick tock
-        if (this.sec > 59) {
-            this.sec = 0
-            this.min += 1
-            if (this.min > 59) {
-                this.min = 0
-                this.hour += 1
+    fun tickDown() {
+        this.sec -= 1 // tick tock tick tock tick tock
+        if (this.sec < 0) {
+            this.sec = 59
+            this.min -= 1
+            if (this.min < 0) {
+                this.min = 59
+                this.hour -= 1
             }
         }
     }
@@ -95,6 +118,27 @@ class TimeCountdown(var sec: Int, var min: Int, var hour: Int) {
         result = 31 * result + min
         result = 31 * result + hour
         return result
+    }
+
+
+}
+
+class CountdownService(val startTime:TimeCountdown) : Service() {
+    var currentTime:TimeCountdown = startTime
+
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
 
